5da1034684351249888be67aa4f550c8c69d5a23
Puskar-Roy <puskarroy600@gmail.com>
2024-04-08 00:16:37 +0530

    feat : file upload

---
+++ src/controllers/profilePicController.ts
@@ -0,0 +1,40 @@
+import cloudinary from 'cloudinary';
+import { v4 as uuidv4 } from 'uuid';
+import { Request as ExpressRequest, Response } from 'express';
+import asyncHandler from '../util/catchAsync';
+import dataUri from '../util/dataUri';
+import UserModel from '../models/userSchema';
+
+cloudinary.v2.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+interface RequestWithFile extends ExpressRequest {
+  file: unknown;
+}
+
+export const createProfilePic = asyncHandler(
+  async (req: RequestWithFile, res: Response) => {
+    const { userId } = req.params;
+    const file = req.file;
+    const fileUri = dataUri(file);
+
+    try {
+      const uploadCloud = await cloudinary.v2.uploader.upload(fileUri.content);
+      console.log(uploadCloud);
+      const existingProfile = await UserModel.findById({ _id: userId });
+      if (!existingProfile) {
+        throw new Error('User did not exist!');
+      }
+      const createProfile = await UserModel.updateOne({
+        profilePic: uploadCloud.secure_url,
+      });
+      res.status(200).json(createProfile);
+    } catch (error) {
+      console.error('Error updating profile pic in database:', error);
+      res.status(500).json({ error: 'Internal server error' });
+    }
+  }
+);
--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,11 +1,14 @@
 import express, { Router } from 'express';
 import { getAllUsers, getUser } from '../controllers/userController';
+
 import { protect } from '../middleware/middleware';
+import { createProfilePic } from '../controllers/profilePicController';
+import fileUpload from '../util/fileUpload';
 
 const router: Router = express.Router();
 
-router.use(protect);
 router.get('/:id', getUser);
 router.get('/', getAllUsers);
+router.post('/:userId/profile', fileUpload, createProfilePic);
 
 export default router;
