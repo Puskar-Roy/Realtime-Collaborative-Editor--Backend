cf5e0f67fa6ea59883963805dd7adbfe5e9dd23c
Atul <oraonatula@gmail.com>
2024-04-25 02:34:11 +0530

    Now the auth Routes are working normally

--- src/routes/oauthRoutes.ts
+++ src/routes/oauthRoutes.ts
@@ -1,38 +1,49 @@
 import express from 'express';
-import express_session from 'express-session';
 
 import passport from '../util/auth/oauth';
 import config from '../config/config';
 
 const router = express.Router();
-
-router.use(
-  express_session({
-    secret: config.SESSION_SECRET || 'BALMER234ui',
-    resave: false,
-    saveUninitialized: false,
-    cookie: {
-      secure: config.DEV_ENV === 'PROD'? true : false,
-      httpOnly: true,
-      sameSite: 'lax',
-      maxAge: 1000 * 60 * 60 * 24 * 7,
-    },
-  })
-)
+interface iRequestWithToken extends express.Request {
+  user: {
+    accessToken: string;
+    refreshToken: string;
+  };
+}
 
 router.get(
   '/google/callback',
   passport.authenticate('google', {
-    successRedirect: config.CLIENT_URL,
+    // successRedirect: config.CLIENT_URL+"/success_auth",
     failureRedirect: '/api/v0.1/auth/register',
-  })
+  }),
+  (req: iRequestWithToken, res) => {
+    try {
+      console.log('google auth callback hit');
+      console.log('req.user: ', req.user);
+
+      // @ts-ignore
+      console.log('tokens are', req.user.accessToken, req.user.refreshToken);
+
+      // Let's send this accessToken to the frontend.
+      res.cookie('accessToken', req.user.accessToken, {
+        httpOnly: true,
+        secure: true,
+        sameSite: 'none',
+      });
+      res.redirect('/success_auth');
+    } catch (error) {
+      console.log(error);
+      res.redirect('/failure_auth');
+    }
+  }
 );
 
 router.get(
   '/google',
   (req, res, next) => {
     console.log('google auth hit');
-    console.log("original url: ", req.originalUrl);
+    console.log('original url: ', req.originalUrl);
     next();
   },
   passport.authenticate('google', { scope: ['profile', 'email'] })
