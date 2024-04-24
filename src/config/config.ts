69b548eb7d350101d2dfcc12abd132e8340d1b7b
Atul <oraonatula@gmail.com>
2024-04-25 00:49:35 +0530

    The session secret has been added

--- src/config/config.ts
+++ src/config/config.ts
@@ -13,8 +13,9 @@ const config = {
   FRONTENDURL: process.env.FRONTENDURL,
   GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
   GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
-  GOOGLE_URL: process.env.GOOGLE_URL,
+  GOOGLE_CALLBACK_URL: process.env.GOOGLE_CALLBACK_URL,
   CLIENT_URL: process.env.CLIENT_URL,
+  SESSION_SECRET: process.env.SESSION_SECRET,
 };
 
 export default config;
